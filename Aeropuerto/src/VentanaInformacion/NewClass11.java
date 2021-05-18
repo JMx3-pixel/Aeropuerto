139b2678ab6edd14a821e13697ce22e48b82dd99
Tirus214 <63261362+Tirus214@users.noreply.github.com>
2021-05-17 19:09:51 -0600

    creacion de aplicaciones

---
+++ Aeropuerto/src/VentanaInformacion/NewClass11.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VentanaInformacion;
+
+import Vuelos.*;
+import VentanaControlador.*;
+
+/**
+ *
+ * @author Jean Paul
+ */
+public class NewClass11 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
---
+++ Aeropuerto/src/Vuelos/NewClass1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vuelos;
+
+import VentanaControlador.*;
+
+/**
+ *
+ * @author Jean Paul
+ */
+public class NewClass1 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
